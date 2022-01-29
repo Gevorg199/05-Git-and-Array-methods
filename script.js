f954ddb0f23ae2e87d947f0f402dc48d8eceef53
Gevorg <ter>
2022-01-29 17:45:24 +0400

    aa

---
+++ script.js
@@ -0,0 +1,5 @@
+// concat Մեթոդ է որը միացնում է զանգվածները
+const myGirls = ["Ani", "Sona"];
+const myBoys = ["Poxos", "Petros"];
+const myChildren = myGirls.concat(myBoys);
+document.getElementById("concat").innerHTML = myChildren
